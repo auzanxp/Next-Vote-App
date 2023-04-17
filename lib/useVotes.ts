7033c3d80847b1af5463f420c5ac0c317ea82441
Auzan <istikhoriauzan@gmail.com>
2023-04-17 23:53:48 +0700

    fix: route API fetcher

--- lib/useVotes.ts
+++ lib/useVotes.ts
@@ -2,7 +2,7 @@ import { votes } from '@prisma/client'
 import useSWR from 'swr'
 
 export default function useVotes() {
-    const fetcher = (url: string) => fetch(url).then((r) => r.json)
+    const fetcher = (url: string) => fetch(url).then((r) => r.json())
     const { data, error } = useSWR<Res<votes[]>>('/api/vote', fetcher)
     
     return {
